a4a31316bf8e70d43a2343b293ae264a705e8355
Rahman Younus <rahman.younus@dazn.com>
2020-12-14 17:20:24 +0000

    wip

--- src/schema.ts
+++ src/schema.ts
@@ -1,13 +1,23 @@
 import * as yup from "yup";
 
-const formValidationSchema = yup.object().shape({
-  lifeExpectancy: yup
-    .number()
-    .min(1)
-    .max(100)
-    .required("Life expectancy is required"),
-  dateOfBirth: yup.string().required("Date of birth is required"),
-});
+const formValidationSchema = yup
+  .object()
+  .shape({
+    lifeExpectancy: yup
+      .number()
+      .min(1)
+      .max(100)
+      .required("Life expectancy is required"),
+    dateOfBirth: yup.string().required("Date of birth is required"),
+  })
+  .test(
+    "test-date-range-valid-for-age",
+    "The data is not correct",
+    function () {
+      // TODO: parse date and see if age < date
+      return this.parent.lifeExpectancy && this.parent.dateOfBirth;
+    }
+  );
 
 const extractErrors = ({ inner }) => {
   return inner.reduce((acc, err) => {
