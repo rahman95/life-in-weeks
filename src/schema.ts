7d5ba68a1dc6ce98124e280a2a48f3de47c0f322
Rahman Younus <rahman_95@live.co.uk>
2020-12-16 21:43:58 +0000

    added custom validation to ensure lifeExpectancy >= age

--- src/schema.ts
+++ src/schema.ts
@@ -6,8 +6,37 @@ const formValidationSchema = yup.object().shape({
     .min(1)
     .max(100)
     .required("Life expectancy is required"),
-  dateOfBirth: yup.string().required("Date of birth is required"),
+  dateOfBirth: yup
+    .string()
+    .test(
+      "is-less-or-equal-to-life-expectancy",
+      "Age should be less or equal to expected life expactancy",
+      function (value) {
+        if (value.length == 0) {
+          return false;
+        }
+
+        const age = getAge(value);
+        const lifeExpectancy = this.parent.lifeExpectancy;
+
+        return lifeExpectancy >= age;
+      }
+    )
+    .required("Date of birth is required"),
 });
+
+const getAge = (dateString) => {
+  const today = new Date();
+  const birthDate = new Date(dateString);
+
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+
+  return age;
+};
 
 const extractErrors = ({ inner }) => {
   return inner.reduce((acc, err) => {
